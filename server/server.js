6738a5112a51a81c654455563c5a45bf29657d3e
ramotk02 <omartaky.ot@gmail.com>
2024-05-04 14:55:24 +0100

    up

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3002;
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://t-tasks.vercel.app/"],
+    origin: ["*"],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true,
   }
